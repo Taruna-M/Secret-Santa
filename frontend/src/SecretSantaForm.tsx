2ef7d937c9e3273aee7b670308a0137e545cfe31
Taruna-M <t.mulugur@gmail.com>
2024-12-25 17:14:30 +0530

    vercel deploy

--- frontend/src/SecretSantaForm.tsx
+++ frontend/src/SecretSantaForm.tsx
@@ -13,7 +13,7 @@ const SecretSantaForm: React.FC = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     axios
-      .patch(`http://localhost:6500/api/assign`, { email })
+      .patch(`https://secret-santa-ten-nu.vercel.app/api/assign`, { email })
       .then((res) => {
         console.log(res.data.response);
         setResponseDetails(res.data.response);
